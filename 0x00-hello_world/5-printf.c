ad935644b9365c908dafde8228e9fd19a733261b
theGraye <niimartei24@gmail.com>
2023-10-05 18:45:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  prints an exact sentence
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
