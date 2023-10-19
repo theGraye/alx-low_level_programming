bdf7c6c232b5cf61977737059e7886eb281b65be
theGraye <niimartei24@gmail.com>
2023-10-19 08:11:00 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,14 @@
 
 char *string_toupper(char *str)
 {
-        int i;
+	int i;
 
-        for (i = 0 ; str[i] != '\0' ; i++)
-        {
-                if (str[i] >= 97 && str[i] <= 122)
-                {
-                        str[i] = str[i] - 32;
-                }
-        }
-        return (str);
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
