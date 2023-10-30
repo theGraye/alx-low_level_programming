1d628ce5a3bbd43935f6bc568cbba0229987be6f
theGraye <niimartei24@gmail.com>
2023-10-30 07:03:14 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,11 @@
  * Return: 0
  */
 
-int main(int agrc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0 ; i < agrc ; i++)
+	for (i = 0 ; i < argc ; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
