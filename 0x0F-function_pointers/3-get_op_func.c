df3697de33389e7f6f79f86ccaa250e6e1756113
theGraye <niimartei24@gmail.com>
2023-11-08 16:56:42 -0800

    Program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects correct function to perform operation
+ * @s: operation argument
+ * Return: result of selected choice
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	/* initialize i */
+	i = 0;
+
+	/* s does not match any of the operands */
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f); /*return operation */
+}
