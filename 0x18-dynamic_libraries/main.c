7f612903c94aef1a9dce8e9f6de0412139bc2709
theGraye <niimartei24@gmail.com>
2024-01-15 06:41:41 -0800

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
 }
